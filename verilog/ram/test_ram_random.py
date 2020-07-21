c5f329a59cbbb4eee8b54259e11e79f094392791
jwrr <jwrr.com@gmail.com>
2020-07-21 00:26:35 -0400

    Tweak test_ram_random.py

--- verilog/ram/test_ram_random.py
+++ verilog/ram/test_ram_random.py
@@ -6,25 +6,51 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
 
+# -----------------------------------------------------------------------------
 
-def dv(dut, act, exp, description):
+def dv_new(dut):
+  dv_state = {"err_cnt" : 0, "tot_cnt" : 0, "err_max" : 10, "dut" : dut}
+  return dv_state
+
+def dv(dv_state, act, exp, description):
     if exp == None:
-        return
-
+        return dv_state
 #     parent = act.parent
-    val = act.value.integer
+    dut = dv_state['dut']
+    dv_state['tot_cnt'] += 1
+    binstr = act.value.binstr
+    try:
+        val = int(binstr,2)
+    except ValueError:
+        dv_state['err_cnt'] += 1
+        dut._log.error("FAIL: act = " + binstr  + " exp = " + hex(exp) + " " + description)
+        return dv_state
     if exp != val:
-        dut._log.error("act = " + hex(val) + " exp = " + hex(exp) + " " + description)
+        dv_state['err_cnt'] += 1
+        dut._log.error("FAIL: act = " + hex(val)  + " exp = " + hex(exp) + " " + description)
+        if dv_state['err_cnt'] >= dv_state['err_max'] :
+            assert False, "TEST FAILED - Fail Count = " + str(dv_state['err_cnt'])
     else:
-        dut._log.info("act = " + hex(val) + " " + description)
+        dut._log.info("PASS: act = " + hex(val) + " " + description)
+
+    return dv_state
+
+def dv_done(dv_state):
+    if dv_state['err_cnt'] > 0:
+        assert False, "TEST FAILED - Fail Count = " + str(dv_state['err_cnt'])
+    else:
+        assert True, "TEST PASSED"
+
+
+# -----------------------------------------------------------------------------
 
 
 @cocotb.test()
 async def test_ram_random(dut):
 
-    print("dut type =", type(dut) )
+    dv_state = dv_new(dut)
 
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.fork(clock.start())  # Start the clock
 
     expect_ram = {}
@@ -46,22 +72,17 @@ async def test_ram_random(dut):
         await FallingEdge(dut.clk)
 
         if i_we == 0:
-            dv(dut, dut.o_dat, rdata_exp , "ram[" + str(i_addr) + "]")
+            dv(dv_state, dut.o_dat, rdata_exp , "ram[" + str(i_addr) + "]")
 
-
-
+    dv_state = {"err_cnt" : 0, "tot_cnt" : 0, "err_max" : 10, "dut" : dut}
     for i in range(256):
         dut.i_we = 0
         dut.i_addr = i
 
+#         if i >= 10:
+#             expect_ram[i] = 0
+
         await FallingEdge(dut.clk)
-        if i == 10:
-            expect_ram[i] = 0
-        dv(dut, dut.o_dat, expect_ram.get(i), "ram[" + str(i) + "]")
-#         exp = expect_ram[i]
-#         act = dut.o_dat.value.integer
-#         if exp != act:
-#           dut._log.error("ram[" + str(i)+ "] exp = " + hex(exp) + " act = " + hex(act) )
-#         else:
-#           dut._log.info("ram[" + str(i)+ "] exp = " + hex(exp) + " act = " + hex(act) )
+        dv(dv_state, dut.o_dat, expect_ram.get(i), "ram[" + str(i) + "]")
+    dv_done(dv_state)
 
