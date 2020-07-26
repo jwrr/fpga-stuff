64488c48fc9a4a45276a8e4946e3a8b4430aad2e
jwrr <jwrr.com@gmail.com>
2020-07-26 00:57:54 -0400

    Add Makefile to compile firmware

--- projects/z80/test_iceZ0mb1e.py
+++ projects/z80/test_iceZ0mb1e.py
@@ -13,7 +13,7 @@ from cocotb.triggers import RisingEdge
 # -----------------------------------------------------------------------------
 
 @cocotb.test()
-async def test_icez0mb1e_directed(dut):
+async def test_icez0mb1e_gpio_loopback(dut):
 
     clk = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.fork(clk.start())  # Start the clock
@@ -22,8 +22,18 @@ async def test_icez0mb1e_directed(dut):
     dut.P1_in = 0x55
     dut.P2_in = 0xAA
 
-    for i in range(5000):
+    for i in range(10000):
         await FallingEdge(dut.clk)
+        binstr = dut.P1_out.value.binstr
+#         dut._log.info("P1_out = " + binstr)
+        try:
+            val = int(binstr,2)
+        except ValueError:
+            val = 0x42
+        dut.P2_in <= val
+        if (i+1) % 1000 == 0:
+            dut._log.info(str(i+1))
 
+    assert val == 0, "Error count from Z80 for GPIO Loopback Test"
 
 
