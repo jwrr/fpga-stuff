5d2c65bc9a864a43dfd5b26c04d266c291d1153c
jwrr <jwrr.com@gmail.com>
2020-07-27 20:14:33 -0400

    Start of spi_rx transactor

--- projects/ram/dv_test.py
+++ projects/ram/dv_test.py
@@ -7,7 +7,7 @@ import cocotb
 
 class dv_test:
 
-    def __init__(self, dut, msg_lvl="Fail", err_max=10):
+    def __init__(self, dut, msg_lvl="Fail", err_max=0):
         self.dut = dut
         msg_lvls = {"None" : 0, "Summary" : 1, "Fail" : 2, "All" : 3}
         self.msg_lvl = msg_lvls[msg_lvl]
@@ -32,7 +32,7 @@ class dv_test:
             self.err_cnt += 1
             if self.msg_lvl >= 2:
                 self.dut._log.error("FAIL: act = " + hex(val)  + " exp = " + hex(exp) + " " + description)
-            if self.err_cnt >= self.err_max:
+            if self.err_max > 0 and self.err_cnt >= self.err_max:
                 self.done()
         else:
             if self.msg_lvl >= 3:
@@ -53,4 +53,34 @@ class dv_test:
 
     def info(self, msg):
         self.dut._log.info(msg)
+
+
+    def val(self, sig, xval = -1):
+        binstr = sig.value.binstr
+        try:
+            val = int(binstr,2)
+            return val
+        except ValueError:
+            return xval
+
+
+    def hi(self, sig, xval = False):
+        binstr = sig.value.binstr
+        try:
+            val = int(binstr,2)
+            return val != 0
+        except ValueError:
+            return xval
+
+
+    def lo(self, sig, xval = False):
+        binstr = sig.value.binstr
+        try:
+            val = int(binstr,2)
+            return val == 0
+        except ValueError:
+            return xval
+
+
+
 
--- projects/z80/test_iceZ0mb1e.py
+++ projects/z80/test_iceZ0mb1e.py
@@ -6,6 +6,8 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
 from cocotb.triggers import RisingEdge
+from cocotb.triggers import ClockCycles
+from dv_test import dv_test
 
 
 # -----------------------------------------------------------------------------
@@ -14,6 +16,7 @@ from cocotb.triggers import RisingEdge
 
 @cocotb.test()
 async def test_icez0mb1e_gpio_loopback(dut):
+    dv = dv_test(dut)
 
     clk = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.fork(clk.start())  # Start the clock
@@ -32,8 +35,37 @@ async def test_icez0mb1e_gpio_loopback(dut):
             val = 0x42
         dut.P2_in <= val
         if (i+1) % 1000 == 0:
-            dut._log.info(str(i+1))
+            dv.info(str(i+1))
 
     assert val == 0, "Error count from Z80 for GPIO Loopback Test"
+
+    dv.info("SPI SHOULD BE READING")
+    
+#     output spi_sclk,
+#     output spi_cs,
+#     output spi_mosi,
+#     input  spi_miso,
+    
+    spi_str = ""
+    prev_clk = dv.val(dut.spi_sclk);
+    for i in range(10000):
+        await FallingEdge(dut.clk)
+        curr_clk = dv.val(dut.spi_sclk);
+        re = curr_clk == 1 and prev_clk == 0
+        fe = curr_clk == 0 and prev_clk == 1
+        edge = re
+        prev_clk = curr_clk
+        if dv.lo(dut.spi_cs):
+            if edge:
+                dv.info( "CS=" + str(dv.val(dut.spi_cs)) + " sclk=" + str(dv.val(dut.spi_sclk)) + " mosi=" + str(dv.val(dut.spi_mosi)) )
+                spi_str = spi_str + str(dv.val(dut.spi_mosi))  
+        elif dv.hi(dut.spi_cs):
+            if spi_str != "":
+                dv.info("spi = " + spi_str)
+            spi_str = "";
+        
+        if (i+1) % 1000 == 0:
+            dv.info(str(i+1))
+#   await ClockCycles(dut.clk,10000)
 
 
